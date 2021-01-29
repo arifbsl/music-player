ec8c84d558021084c5743a1876a7b896da061a32
arifbsl <ahak.bsl@gmail.com>
2021-01-29 12:16:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');l
+const artist = document.getElementById('artist');l
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+
+// condition
+let isPlaying = false;
+
+
+// play
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', "Pause");
+    
+    music.play();
+}
+// pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', "Play");
+    music.pause();
+}
+
+
+// Play / Pause Event
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
