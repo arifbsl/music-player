b6cb2d58e4e06429fd92cf2b39b01623713bc32c
arifbsl <ahak.bsl@gmail.com>
2021-01-29 14:04:09 +0600

    controles button are done 100% working:

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const image = document.querySelector('img');
-const title = document.getElementById('title');l
-const artist = document.getElementById('artist');l
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Music 1',
+        artist: "Jacinto Desiner 1",
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Electric Music 2',
+        artist: "Jacinto Desiner 2",
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Electric Music 3',
+        artist: "Jacinto Desiner 3",
+    }
+];
 
-// condition
+// Check if playing
 let isPlaying = false;
 
 
@@ -30,3 +48,40 @@ function pauseSong() {
 
 // Play / Pause Event
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+// Previous song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+// Next song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Load the Music and data
+loadSong(songs[2]);
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
