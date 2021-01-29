90f330381f520abe4cd2479e385870409b6c6e13
arifbsl <ahak.bsl@gmail.com>
2021-01-29 14:23:21 +0600

    progress bar now dynamic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -81,7 +83,17 @@ function nextSong() {
 
 // Load the Music and data
 loadSong(songs[2]);
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const {duration, currentTime} = e.srcElement;
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
